99822522864e56ff09fd52fd8a664eebbb2f0624
Markus Reiter-Haas <reiter-haas@student.tugraz.at>
2016-01-30 15:49:37 +0100

    GoatThrower improvements

--- Assets/Scripts/GoatThrower.cs
+++ Assets/Scripts/GoatThrower.cs
@@ -3,15 +3,29 @@ using System.Collections;
 
 public class GoatThrower : MonoBehaviour {
 
+	public Vector3 direction;
+
 	// Use this for initialization
 	void Start () {
+		StartCoroutine ("ThrowInterval");
+	}
+
+	void ThrowGoat() {
 		GameObject prefab = GameObject.Find ("Goat");
-		GameObject obj = (GameObject)Instantiate (prefab, new Vector3(0, 4, 0), Quaternion.identity);
-		obj.GetComponent<Rigidbody> ().AddForce (new Vector3 (100, 0, 0));
+		GameObject obj = (GameObject)Instantiate (prefab, transform.position, Quaternion.identity);
+		obj.GetComponent<Rigidbody> ().AddForce (direction);
+	}
+
+	IEnumerator ThrowInterval() {
+		while (true) {
+			float waittime = Random.Range(0.4f, 1.3f);
+			yield return new WaitForSeconds (waittime);
+			ThrowGoat ();
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
