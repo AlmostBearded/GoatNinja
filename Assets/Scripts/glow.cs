9b56a0f41c898b80ddcde1ec1bd380f07b08b189
Stephan Stiboller <stiboller@gmail.com>
2016-01-31 13:13:50 +0200

    fading pentagram

---
+++ Assets/Scripts/glow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class glow : MonoBehaviour {
+
+    public Material[] materials;
+    public float changeInterval = 0.33F;
+
+    public Renderer renderer;
+
+    public int highscore;
+
+	void Start ()
+    {
+        renderer = GameObject.FindGameObjectWithTag("glow").GetComponent<MeshRenderer>();
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+    }
+
+    void Update ()
+    {
+        int currentHighscore = PlayerPrefs.GetInt("lastscore", 0);
+        float alpha = 100 * currentHighscore / highscore;
+        alpha = alpha * 0.01f;
+        Color color = renderer.materials[1].color;
+        color.a = alpha;
+        renderer.materials[1].color = color;
+    }
+}
