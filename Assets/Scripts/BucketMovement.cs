8a343761f3cd3e15366c2e1a20c198fc023bcb3d
Peter Oberrauner <oberrauner.peter@gmx.at>
2016-01-31 11:25:26 +0100

    Made the bucket follow the mouse.

--- Assets/Scripts/BucketMovement.cs
+++ Assets/Scripts/BucketMovement.cs
@@ -2,48 +2,66 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
-public class BucketMovement : MonoBehaviour {
+public class BucketMovement : MonoBehaviour
+{
 
-	private GameObject bucket;
-	private Vector3 leftpoint;
-	private Vector3 rightpoint;
-	private bool goRight;
-	private float speed;
+  private GameObject bucket;
+  private Vector3 leftpoint;
+  private Vector3 rightpoint;
+  private bool goRight;
+  private float speed;
 
-	private int counter = 0;
-	private Text scoreUI;
+  private int counter = 0;
+  private Text scoreUI;
+  private Plane playArea;
 
-	// Use this for initialization
-	void Start () {
-		bucket = GameObject.FindGameObjectWithTag ("Bucket");
-		leftpoint = new Vector3 (-10, 0, 0);
-		rightpoint = new Vector3 (10, 0, 0); 
-		goRight = true;
-		speed = 0.1f;
-		scoreUI = GameObject.FindGameObjectWithTag ("Score").GetComponent<Text>();
-	}
-		void FixedUpdate () {
-		Vector3 curr = bucket.transform.position;
+  public void Awake()
+  {
+    playArea = new Plane(new Vector3(0, 0, -1), 0);
+    bucket = GameObject.FindGameObjectWithTag("Bucket");
+    leftpoint = new Vector3(-10, 0, 0);
+    rightpoint = new Vector3(10, 0, 0);
+    goRight = true;
+    speed = 0.08f;
+    scoreUI = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+  }
 
-		if (goRight)
-			bucket.transform.position += new Vector3 (speed, 0, 0);
-		else
-			bucket.transform.position -= new Vector3 (speed, 0, 0);
-
-		if (curr.x > rightpoint.x)
-			goRight = false;
-
-		if (curr.x < leftpoint.x)
-			goRight = true;
-
-        scoreUI.text = "Score: " + counter;
-		PlayerPrefs.SetInt ("lastscore", counter);
+  void FixedUpdate()
+  {
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    float playAreaDistance;
+    playArea.Raycast(ray, out playAreaDistance);
+    Vector3 playAreaIntersection = ray.GetPoint(playAreaDistance);
 
 
-    }
+    Vector3 curr = bucket.transform.position;
 
-	void OnParticleCollision(GameObject other) {
-		counter++;
-		//Debug.Log ("Hit" + counter);
-	}
+    float rightPoint = playAreaIntersection.x + 2;
+    float leftPoint = playAreaIntersection.x - 2;
+
+    if (bucket.transform.position.x >= rightPoint)
+      goRight = false;
+
+    if (bucket.transform.position.x < leftPoint)
+      goRight = true;
+
+    bucket.transform.position += new Vector3(goRight ? speed : -speed, 0, 0);
+
+    //if (curr.x > rightpoint.x)
+    //  goRight = false;
+
+    //if (curr.x < leftpoint.x)
+    //  goRight = true;
+
+    scoreUI.text = "Score: " + counter;
+    PlayerPrefs.SetInt("lastscore", counter);
+
+
+  }
+
+  void OnParticleCollision(GameObject other)
+  {
+    counter++;
+    //Debug.Log ("Hit" + counter);
+  }
 }
